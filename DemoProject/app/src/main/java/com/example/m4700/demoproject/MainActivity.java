ccaf060b72407c89f513040fd35718708252b99a
Nguyen Hoang Anh <ng.hoanganh8x@gmail.com>
2017-05-24 17:07:47 +0700

    commit

--- DemoProject/app/src/main/java/com/example/m4700/demoproject/MainActivity.java
+++ DemoProject/app/src/main/java/com/example/m4700/demoproject/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gridlayout_sample01);
+        setContentView(R.layout.exercise_car_lt_04);
     }
 }
